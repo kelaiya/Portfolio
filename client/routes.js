171492b8c24e277a7903de22884b66e417ba74ed
Kelaiya <kelaiyarao1@gmail.com>
2018-03-05 22:10:21 -0500

    client added

---
+++ client/routes.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {Route, Switch, Router} from 'react-router-dom'
+import PropTypes from 'prop-types'
+import history from './history'
+import {Main, Projects} from './components'
+import {contact} from './store'
+
+/**
+ * COMPONENT
+ */
+export default class Routes extends Component {
+  render () {
+    return (
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/home" component={Main} />
+          <Route exact path="/projects" component={Projects} />
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+
---
+++ client/store/index.js
@@ -0,0 +1,15 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import createLogger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import contact from './contact'
+
+const reducer = combineReducers({contact})
+const middleware = composeWithDevTools(applyMiddleware(
+  thunkMiddleware,
+  createLogger({collapsed: true})
+))
+const store = createStore(reducer, middleware)
+
+export default store
+export * from './contact.js'
