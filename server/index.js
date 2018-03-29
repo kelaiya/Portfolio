5c76238e78647f0f45ee4dedbd05f036b11deb92
Kelaiya <kelaiyarao1@gmail.com>
2018-03-29 01:30:18 -0400

    changes

--- server/index.js
+++ server/index.js
@@ -21,6 +21,10 @@ const createApp = () => {
   // static file-serving middleware
   app.use(express.static(path.join(__dirname, '..', 'public')))
 
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+  })
+  
   // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
     if (path.extname(req.path).length) {
